52eb46bef4e30d9e2d4746a035f0ec8d1399e849
Omar Said Kamel Ahmed <75030145+omarsaidkamel@users.noreply.github.com>
2024-07-24 21:22:37 +0200

    Create Arithmeticoperationproblem.cpp

---
+++ Second Lecture/Arithmeticoperationproblem.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+  long long int x,y;
+  cin>>x>>y;
+  cout<<<<x<<" + "<<y<<x+y<<"\n";
+  cout<<<<x<<" * "<<y<<x*y<<"\n";
+  cout<<<<x<<" - "<<y<<x-y<<"\n";
+  return 0;
+}
