acf875dad51bf00a57ba23f99c4e3f31e93d69c0
Omar Said Kamel Ahmed <75030145+omarsaidkamel@users.noreply.github.com>
2024-07-24 21:30:59 +0200

    Create Separate four-digit problem.cpp

---
+++ Second Lecture/Separate four-digit problem.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int i;
+  cin>>i;
+  cout<<i/1000<<"\n";
+  cout<<(i/100)%10<<"\n";
+  cout<<(i/10)%10<<"\n";
+  cout<<i%10<<"\n";
+  return 0;
+}
